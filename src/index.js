33b8aec167ba30566b974685abea9626e2067969
Anparasan3 <anparasantamilarasan@gmail.com>
2025-11-21 15:09:23 +0530

    refactor serverless handler logic for Vercel deployment

--- src/index.js
+++ src/index.js
@@ -14,53 +14,39 @@ const ensureDb = async () => {
   return mongoConnectPromise;
 };
 
-// If running on Vercel (serverless), export a request handler instead of starting a server
-if (process.env.VERCEL) {
-  module.exports = async (req, res) => {
-    try {
-      await ensureDb();
-      return app(req, res);
-    } catch (err) {
-      // eslint-disable-next-line no-console
-      console.error('Serverless handler error', err && err.message ? err.message : err);
-      res.statusCode = 500;
-      res.end('Internal Server Error');
-    }
-  };
-} else {
-  // Local / traditional server start (unchanged)
-  let server;
-  ensureDb().then(() => {
+let server;
+ensureDb()
+  .then(() => {
     server = app.listen(config.port, () => {
       logger.info(`Listening to port ${config.port}`);
     });
-  }).catch((err) => {
+  })
+  .catch((err) => {
     logger.error('Failed to connect to MongoDB', err);
   });
 
-  const exitHandler = () => {
-    if (server) {
-      server.close(() => {
-        logger.info('Server closed');
-        process.exit(1);
-      });
-    } else {
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      logger.info('Server closed');
       process.exit(1);
-    }
-  };
+    });
+  } else {
+    process.exit(1);
+  }
+};
 
-  const unexpectedErrorHandler = (error) => {
-    logger.error(error);
-    exitHandler();
-  };
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
+  exitHandler();
+};
 
-  process.on('uncaughtException', unexpectedErrorHandler);
-  process.on('unhandledRejection', unexpectedErrorHandler);
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
 
-  process.on('SIGTERM', () => {
-    logger.info('SIGTERM received');
-    if (server) {
-      server.close();
-    }
-  });
-}
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM received');
+  if (server) {
+    server.close();
+  }
+});
